c7bc05b67232b6e7dc571411e5a67639719b7b2a
vanityteen <lyj19990107@qq.com>
2021-08-26 12:03:22 +0800

    Updates

--- demo01/README.md
+++
@@ -1,5 +0,0 @@
-# train
-企业实训练习演示
-
-## a testing
-## only simple page to page
--- demo05/dist/js/newsList.js
+++
@@ -1,1 +0,0 @@
-"use strict";var _axios=_interopRequireDefault(require("axios"));function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}var res=(0,_axios.default)({method:"GET",url:"https://www.mxnzp.com/api/news/list",params:{typeId:"515",page:1,app_id:"emfjlekgxchngglp",app_secret:"NStsd016Q0dtdEJMclpUbytualF5Zz09"}});module.export={res:res};
